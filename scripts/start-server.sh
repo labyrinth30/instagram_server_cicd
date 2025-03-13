73805a419a6ee34f584c189f7423ac38901466f1
younha00 <ayeon0@office.skhu.ac.kr>
2025-03-13 15:22:13 +0900

    프로젝트 이름 수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/instagram_server_cicd
+cd /home/ubuntu/instagram_server
 npm i
 pm2 kill
 pm2 start dist/main.js --name "backend-server"
