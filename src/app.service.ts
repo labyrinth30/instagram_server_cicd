1497f430bed979b2d6886f76c532c026d9a328b3
younha00 <ayeon0@office.skhu.ac.kr>
2025-03-13 12:41:05 +0900

    ConfigModule 추가

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { env } from 'process';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log(process.env.DB_NAME);
-    console.log('getHello');
-    return `${process.env.DB_NAME}`;
+    return `${process.env.DB_NAME};`;
   }
 }
