19afb50c0ab486595b2d2015d7316e61266f87f0
younha00 <ayeon0@office.skhu.ac.kr>
2025-03-13 22:10:38 +0900

    Code Deploy 추가

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/instagram_server
-npm i
-pm2 kill
-pm2 start dist/main.js --name "backend-server"
-echo "--------------- 서버 배포 끝 -----------------"
+docker stop instagram-server || true
+docker rm instagram-server || true
+docker pull 390844778157.dkr.ecr.ap-northeast-2.amazonaws.com/instagram-server:latest
+docker run -d --name instagram-server -p 3000:3000 390844778157.dkr.ecr.ap-northeast-2.amazonaws.com/instagram-server:latest
+echo "--------------- 서버 배포 끝 ------------------"
