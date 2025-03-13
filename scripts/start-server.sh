c71ff0ffec6350124f28d5fb736464c9a8463f7f
younha00 <ayeon0@office.skhu.ac.kr>
2025-03-13 14:41:16 +0900

    Code Deploy 추가

---
+++ scripts/start-server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "--------------- 서버 배포 시작 -----------------"
+cd /home/ubuntu/instagram_server_cicd
+npm i
+pm2 kill
+pm2 start dist/main.js --name "backend-server"
+echo "--------------- 서버 배포 끝 -----------------"
